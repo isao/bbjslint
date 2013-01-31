1f2f433751d9b66b54efa79814c136429aafed64
Isao Yagi <isao@yahoo-inc.com>
2013-01-30 18:48:59 -0800

    refactor to use bbresult

--- index.js
+++ index.js
@@ -4,78 +4,26 @@
 
 var fs = require('fs'),
     lint = require('jslint/lib/linter').lint,
-    ascr = require('applescript'),
-    note = require('terminal-notifier'),
+    bbresults = require('bbresults'),
+    pathname = process.env.BB_DOC_PATH;
 
-    fref = process.env.BB_DOC_PATH,
-    fname = process.env.BB_DOC_NAME,
-    TITLE = 'bbedit jslint';
-
-
-function logerr(err) {
-    if (err) {
-        console.log(err);
-    }
-}
-
-function errorObj(results) {
-    var out = [];
-    results.forEach(function (res) {
-        var item = [
-                '{result_kind: "Error"',
-                'result_file: "' + fref + '"',
-                'result_line: ' + res.line,
-                'message: "' + res.reason.replace(/"/g, '\\"') + '"}' //escape
-            ].join();
-        out.push(item);
-    });
-    return '{' + out.join() + '}';
-}
-
-function errorScriptStr(listobj, fname) {
-    return [
-        'tell application "BBEdit"',
-        'set errs to ' + listobj,
-        'make new results browser with data errs with properties {name:"' + TITLE +'"}',
-        'end tell'
-    ].join('\n');
-}
 
 function run(err, str) {
-    var results;
+    var title = 'JSLint results',
+        results;
+
     if (err) {
-        note("error. couldn't read bbedit document.", {title: TITLE});
+        bbresults.notify('error, reading ' + pathname, {title: title});
     } else {
         results = lint(str);
         if (results.ok) {
-            note('no lint in ' + fname, {title: TITLE});
+            bbresults.notify(pathname + ' is lint free', {title: title});
         } else {
-            ascr.execString(errorScriptStr(errorObj(results.errors)), logerr);
+            bbresults.show(results.errors, pathname, title);
         }
     }
 }
 
 if (require.main === module) {
-    fs.readFile(fref, 'utf-8', run);
+    fs.readFile(pathname, 'utf-8', run);
 }
-
-/*
-    > var lint = require('jslint/lib/linter').lint
-    undefined
-    > lint("var a")
-    { functions: [],
-      errors:
-       [ { id: '(error)',
-           raw: 'Expected \'{a}\' and instead saw \'{b}\'.',
-           evidence: 'var a',
-           line: 1,
-           character: 6,
-           a: ';',
-           b: '(end)',
-           c: undefined,
-           d: undefined,
-           reason: 'Expected \';\' and instead saw \'(end)\'.' } ],
-      globals: [ 'a' ],
-      ok: false,
-      options: { node: true, es5: true } }
-*/
